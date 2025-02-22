21093231f92de6a0b9cf76add0d9813e7d208fce
Bmorris25 <brittany2morris@gmail.com>
2025-02-22 14:43:23 -0800

    new tests to get vercel to work

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",  // Use "./" instead of "/"
+  base: "./",  
   build: {
     outDir: "dist",  // Ensure this is correct
   }
