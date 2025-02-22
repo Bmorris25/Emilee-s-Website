c3f93ff0c4eaf5b32a63c33cc2e6dadbfc767921
Bmorris25 <brittany2morris@gmail.com>
2025-02-22 15:46:08 -0800

    new try to fix routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,28 +1,27 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from '../components/navbar'
-import './App.css'
-import Home from '../components/home'
-import Featured from '../components/featured'
-import Footer from '../components/footer'
-import Contact from "../components/contact"
-import Services from '../components/services'
+import Navbar from '../components/navbar';
+import './App.css';
+import Home from '../components/home';
+import Featured from '../components/featured';
+import Footer from '../components/footer';
+import Contact from "../components/contact";
+import Services from '../components/services';
 
 const App = () => {
 
   return (
-    <>
+    <Router>
       <Navbar />
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/featured" element={<Featured />} />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/featured" element={<Featured />} />
+      </Routes>
       <Footer />
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
+
