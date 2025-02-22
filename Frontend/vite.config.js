1e34e675780ded976ec26668b91da3f3d8f3a074
Bmorris25 <brittany2morris@gmail.com>
2025-02-22 15:10:40 -0800

    one last try to update /

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./", // Ensures assets are referenced correctly
+  base: "/", // Ensures assets are referenced correctly
   build: {
     outDir: "dist", // Ensure this matches the deployment output folder
     rollupOptions: {
