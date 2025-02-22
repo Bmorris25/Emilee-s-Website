f837b2876f8d077fbdac59a32dc1555262d404e2
Bmorris25 <brittany2morris@gmail.com>
2025-02-21 20:56:31 -0800

    frontend and SEO done

--- Frontend/components/footer.jsx
+++ Frontend/components/footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div>
         <div className="container">
             <footer className="d-flex flex-wrap justify-content-between align-items-center py-2 my-3 border-top">
-              <p className="col-md-4 mb-0 text-body-secondary">© {new Date().getFullYear()} Emilee Irwin</p>
+              <p className="col-md-4 mb-0 text-body-secondary">© {new Date().getFullYear()} Emilee Irwin | The Desert Vet Grooming Salon | 470 State St, Hurricane, UT 84737 | 📞(435) 635-7000</p>
           
               <a href="/" className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
                 <svg className="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
